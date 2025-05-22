ad188dcadf9b0050fe1d568249048f0e76c65f04
Samuel Belo <samuel.belo@iesde.com.br>
2025-05-22 07:55:56 -0300

    fix: progress on authentication use cases

--- internal/domain/usecase/user/user_service.go
+++ internal/domain/usecase/user/user_service.go
@@ -1,11 +1,28 @@
 package user
 
-import "go-clean-arch/internal/adapter/repository"
+import (
+	"go-clean-arch/internal/adapter/repository"
+)
 
 type UserUseCase interface {
 	SigIn(request UserSignInRequest) (*SignInResponse, error)
+	LogIn(request UserLogInRequest) (*UserLogInResponse, error)
 }
 
 type UserService struct {
 	repository repository.UserRepository
+}
+
+func NewUserService(repo repository.UserRepository) UserUseCase {
+	return &UserService{
+		repository: repo,
+	}
+}
+
+func (u *UserService) SigIn(request UserSignInRequest) (*SignInResponse, error) {
+	panic("unimplemented")
+}
+
+func (u *UserService) LogIn(request UserLogInRequest) (*UserLogInResponse, error) {
+	panic("unimplemented")
 }
